23bb5097f9d76ba1b1320a98d402a3cd8b7bf962
Asen Lekov <asenlekoff@gmail.com>
2015-12-24 00:21:14 +0200

    Add filtering by query params for feed items
    Can be filtering by updated or created date and time

--- server/api/feed/views.py
+++ server/api/feed/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework import filters
 
 from api.feed.models import FeedItem
 from api.feed.serializers import FeedItemSerializer
+from api.feed.filters import FeedItemFilter
 
 
 class FeedViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,5 @@ class FeedViewSet(viewsets.ModelViewSet):
     """
     queryset = FeedItem.objects.all()
     serializer_class = FeedItemSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = FeedItemFilter
