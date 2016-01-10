de33da98abecfbe67b32c166577f3c052349660f
Asen Lekov <asenlekoff@gmail.com>
2016-01-10 20:08:10 +0200

    Add initial feedback models

---
+++ server/api/feedback/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class EventTrackLevelRate(models.Model):
+    event_track_level = models.ForeignKey('events.EventTrackLevel')
+    person = models.ForeignKey('members.Person')
+
+    feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    comment = models.TextField(max_length=8*1024)
+
+
+class ScheduleItemRate(models.Model):
+    schedule_item = models.ForeignKey('events.ScheduleItem')
+    person = models.ForeignKey('members.Person')
+
+    feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    comment = models.TextField(max_length=8*1024)
