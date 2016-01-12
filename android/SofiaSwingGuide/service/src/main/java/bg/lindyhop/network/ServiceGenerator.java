6219b682adf76ab5f0257a8aacda4fab02ee8268
Miroslav <miroslavmironov@gmail.com>
2016-01-12 16:56:56 +0200

    Added a method for creating OAuth services.

--- android/SofiaSwingGuide/service/src/main/java/bg/lindyhop/network/ServiceGenerator.java
+++ android/SofiaSwingGuide/service/src/main/java/bg/lindyhop/network/ServiceGenerator.java
@@ -5,6 +5,7 @@ import android.util.Base64;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import bg.lindyhop.entities.AuthToken;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.converter.GsonConverter;
@@ -32,6 +33,28 @@ public class ServiceGenerator {
                     request.addHeader("Authorization", basic);
                     request.addHeader("Accept", "application/json");
                     request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+                }
+            });
+        }
+
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                .create();
+
+        RestAdapter adapter = builder.setConverter(new GsonConverter(gson)).build();
+        return adapter.create(serviceClass);
+    }
+
+    public static <S> S createService(Class<S> serviceClass, final AuthToken token) {
+
+        if (token != null) {
+            builder.setRequestInterceptor(new RequestInterceptor() {
+                @Override
+                public void intercept(RequestFacade request) {
+                    request.addHeader("Accept", "application/json");
+                    request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+                    request.addHeader("Authorization",
+                                    token.getTokenType() + " " + token.getAccess_token());
                 }
             });
         }
