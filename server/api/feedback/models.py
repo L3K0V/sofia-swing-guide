38c549caa17538d7e9c3def0c448d03ceee1f68b
Asen Lekov <asenlekoff@gmail.com>
2016-01-16 12:46:09 +0200

    Update feedback model 0-100 for percantage.

--- server/api/feedback/models.py
+++ server/api/feedback/models.py
@@ -6,7 +6,7 @@ class EventTrackLevelRate(models.Model):
     event_track_level = models.ForeignKey('events.EventTrackLevel')
     person = models.ForeignKey('members.Person')
 
-    feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     comment = models.TextField(max_length=8*1024)
 
 
@@ -14,5 +14,5 @@ class ScheduleItemRate(models.Model):
     schedule_item = models.ForeignKey('events.ScheduleItem')
     person = models.ForeignKey('members.Person')
 
-    feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     comment = models.TextField(max_length=8*1024)
