5631477ac7f5e12b2e7c0cd8c78edf46a4cea122
Asen Lekov <asenlekoff@gmail.com>
2016-01-22 20:30:28 +0200

    Deployment fixes

--- server/swing/wsgi.py
+++ server/swing/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/sofiaswing/server/')
+sys.path.append('/var/www/sofiaswing/server/lib/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swing.settings")
 
