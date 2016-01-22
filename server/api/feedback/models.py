dd1eadbc23d89e17a7dfe892ed44c1e0c714ba81
Asen Lekov <asenlekoff@gmail.com>
2016-01-22 15:12:19 +0200

    Closes #3

--- server/api/feedback/models.py
+++ server/api/feedback/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class EventTrackLevelRate(models.Model):
     event_track_level = models.ForeignKey('events.EventTrackLevel')
-    person = models.ForeignKey('members.Person')
+    email = models.EmailField(unique=True)
 
     feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     comment = models.TextField(max_length=8*1024)
@@ -12,7 +12,7 @@ class EventTrackLevelRate(models.Model):
 
 class ScheduleItemRate(models.Model):
     schedule_item = models.ForeignKey('events.ScheduleItem')
-    person = models.ForeignKey('members.Person')
+    email = models.EmailField(unique=True)
 
     feedback = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     comment = models.TextField(max_length=8*1024)
