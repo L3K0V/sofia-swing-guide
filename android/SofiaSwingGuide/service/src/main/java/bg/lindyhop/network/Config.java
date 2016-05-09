4d5123fb579ffc590d860a6aa194d53051ffddbc
Miroslav <miroslavmironov@gmail.com>
2016-05-09 23:54:36 +0300

    Changed the oauth parameters.

--- android/SofiaSwingGuide/service/src/main/java/bg/lindyhop/network/Config.java
+++ android/SofiaSwingGuide/service/src/main/java/bg/lindyhop/network/Config.java
@@ -7,7 +7,7 @@ public class Config {
 
     public static final String SERVER_URL = "http://ssdf.tempest.bg/";
 
-    public static final String CLIENT_ID = "ChX2ZtRVK3WBMoa3XT12qbxQMMD0PLVcGmzRr4Pj";
-    public static final String CLIENT_SECRET = "44BFcvVE0WYjMM7rXLHEKJ8LyquEDqbOMcQC0n9h8bC6HIn2TSOwFvnTkV3gNVOgxy316bs42TADclM0MzSfPrsY0yts37bqCFGBlR44BYF31ffscGaY3TxNgEZP7D8u";
+    public static final String CLIENT_ID = "24kdPsiqng8AX8UcqCNijNaXxEgZOvVsWbXjubce";
+    public static final String CLIENT_SECRET = "fNUmuPruEcr3lCeSW0pBoIoiQDrpxSR9pRpPK3z1f5N9eZpV7G7md5YdYe8opmPDYyvW3IzRut8dbFeITAFf6bUJhepazhgwd0fh4MZFcswZXehoTHTi37Ws2nVnGLw3";
     public static final String GRANT_TYPE = "client_credentials";
 }
